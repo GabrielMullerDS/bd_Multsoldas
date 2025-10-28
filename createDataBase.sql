0fdc5994699a65d9f88609ca764454fe875b77d9
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2025-10-28 16:37:02 -0300

    Update createDataBase.sql
    Ajuste de nomes, ordens e criação de um possível rastreamento de patrimônio.
    ---
    Adjustment of names, order, and creation of a possible asset tracking system.

--- createDataBase.sql
+++ createDataBase.sql
@@ -11,41 +11,47 @@ create table tbl_clientes( --criar a tabela tbl_clientes;
 	id_cliente int primary key,
     nome varchar(50) not null,
     cpf_cnpj varchar(14) not null unique,
-    cep varchar(8) not null,
-    rua varchar(50) not null,
-    num varchar(6) not null,
-    complemento varchar(10),
-    bairro varchar(20) not null,
-    cidade varchar(20) not null,
-    uf varchar(2) not null
-);
-create table tbl_pEntrega(
-	id_pEntrega int primary key,
     cep char(8) not null,
     rua varchar(50) not null,
     num varchar(6) not null,
     complemento varchar(10),
     bairro varchar(20) not null,
     cidade varchar(20) not null,
-    uf char(2) not null,
+    uf char(2) not null
+);
+create table tbl_pEntrega(
+	id_pEntrega int primary key,,
     id_cliente int,
-    foreign key (id_cliente) references tbl_clientes(id_cliente)
+    foreign key (id_cliente) references tbl_clientes(id_cliente),
+    ponto varchar(10) not null,
+    cep char(8) not null,
+    rua varchar(50) not null,
+    num varchar(6) not null,
+    complemento varchar(10),
+    bairro varchar(20) not null,
+    cidade varchar(20) not null,
+    uf char(2) not null
 );
 create table tbl_cilindros(
 	id_cilindro int primary key,
     cilindro varchar(20) not null,
     tamanho float(6, 2) not null,
-    marca varchar(15) not null
+    marca varchar(15) not null,
+    empresa enum('Atms', 'AirP', 'Mult') not null,
+    num_serial int(6) not null
 );
 create table tbl_gases(
 	id_gas int primary key,
     gas varchar(20) not null,
     volume varchar(5),
-	id_cilindro int, --esqueci ;-;
+	id_cilindro int,
     foreign key (id_cilindro) references tbl_cilindros(id_cilindro)
 );
-create table tbl_documentos(
-	id_pedido int primary key,
-	id_cliente int, --esqueci tbm ;-;
-    foreign key (id_cliente) references tbl_clientes(id_cliente)
+create table tbl_pedidos(
+	id_pedido int primary key,,
+    dt_pedido datetime default (now()) not null,
+	id_cliente int,
+    foreign key (id_cliente) references tbl_clientes(id_cliente),
+    id_cilindro int,
+    foreign key (id_cilindro) references tbl_cilindros(id_cilindro)
 );
